ab0ebafcb4c98c2328e3245e5b02da9b1663c2f8
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-12-17 23:21:11 +0800

    disable Feature1

--- src/templates/interior-design/Homepage.tsx
+++ src/templates/interior-design/Homepage.tsx
@@ -2,7 +2,7 @@ import { component$ } from '@builder.io/qwik'
 
 import DoubleMarqueeHero from '~/components/hero/DoubleMarqueeHero/DoubleMarqueeHero'
 import LogoMarquee from '~/components/awards/LogoMarquee'
-import Feature1 from '~/components/feature/Feature1'
+// import Feature1 from '~/components/feature/Feature1'
 import Feature2 from '~/components/feature/Feature2'
 import Portfolio1 from '~/components/portfolio/portfolio1/Portfolio1'
 
@@ -13,7 +13,7 @@ export default component$(() => {
       <DoubleMarqueeHero />
       <main>
         <LogoMarquee />
-        <Feature1 />
+        {/* <Feature1 /> */}
         <Feature2 />
         <Portfolio1 />
       </main>
