bd4997e3d5a171f85fd311b8f76e863f1714819b
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-12-17 22:41:51 +0800

    create awards/LogoMarquee

--- src/templates/interior-design/Homepage.tsx
+++ src/templates/interior-design/Homepage.tsx
@@ -1,11 +1,15 @@
 import { component$ } from '@builder.io/qwik'
+import LogoMarquee from '~/components/awards/LogoMarquee'
+
 import DoubleMarqueeHero from '~/components/hero/DoubleMarqueeHero/DoubleMarqueeHero'
 
 export default component$(() => {
   return (
     <>
       <DoubleMarqueeHero />
-      <p>This is a homepage</p>
+      <main>
+        <LogoMarquee />
+      </main>
     </>
   )
 })
