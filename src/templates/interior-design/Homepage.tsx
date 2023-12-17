f5a23d794aafd8636c44c872299d208ccdd37321
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-12-17 23:53:38 +0800

    remvoe Feature1 and create Feature3

--- src/templates/interior-design/Homepage.tsx
+++ src/templates/interior-design/Homepage.tsx
@@ -2,8 +2,8 @@ import { component$ } from '@builder.io/qwik'
 
 import DoubleMarqueeHero from '~/components/hero/DoubleMarqueeHero/DoubleMarqueeHero'
 import LogoMarquee from '~/components/awards/LogoMarquee'
-import Feature1 from '~/components/feature/Feature1'
 import Feature2 from '~/components/feature/Feature2'
+import Feature3 from '~/components/feature/Feature3'
 import Portfolio1 from '~/components/portfolio/portfolio1/Portfolio1'
 import Testimonials1 from '~/components/testimonials/testimonial1/Testimonials1'
 
@@ -13,7 +13,7 @@ export default component$(() => {
       <DoubleMarqueeHero />
       <main>
         <LogoMarquee />
-        <Feature1 />
+        <Feature3 />
         <Feature2 />
         <Portfolio1 />
         <Testimonials1 />
