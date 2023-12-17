939afcc88e614c31c81f176df0c5f12481e9efc0
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-12-18 03:27:53 +0800

    import Form1 to template/Homepage

--- src/templates/interior-design/Homepage.tsx
+++ src/templates/interior-design/Homepage.tsx
@@ -8,6 +8,7 @@ import Portfolio1 from '~/components/portfolio/portfolio1/Portfolio1'
 import Testimonials1 from '~/components/testimonials/testimonial1/Testimonials1'
 import FoundersNote1 from '~/components/FoundersNote/FoundersNote1'
 import Faq1 from '~/components/faq/faq1/Faq1'
+import Form1 from '~/components/form/Form1'
 
 export default component$(() => {
   return (
@@ -20,6 +21,7 @@ export default component$(() => {
         <Portfolio1 />
         <Testimonials1 />
         <Faq1 />
+        <Form1 />
         <FoundersNote1 />
       </main>
     </>
