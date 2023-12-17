61eb5a0e1f79cdae3dc2bd4f671109a0f8278f37
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-12-17 23:18:05 +0800

    enable Feature1

--- src/templates/interior-design/Homepage.tsx
+++ src/templates/interior-design/Homepage.tsx
@@ -2,9 +2,10 @@ import { component$ } from '@builder.io/qwik'
 
 import DoubleMarqueeHero from '~/components/hero/DoubleMarqueeHero/DoubleMarqueeHero'
 import LogoMarquee from '~/components/awards/LogoMarquee'
-// import Feature1 from '~/components/feature/Feature1'
+import Feature1 from '~/components/feature/Feature1'
 import Feature2 from '~/components/feature/Feature2'
 import Portfolio1 from '~/components/portfolio/portfolio1/Portfolio1'
+
 
 export default component$(() => {
   return (
@@ -12,7 +13,7 @@ export default component$(() => {
       <DoubleMarqueeHero />
       <main>
         <LogoMarquee />
-        {/* <Feature1 /> */}
+        <Feature1 />
         <Feature2 />
         <Portfolio1 />
       </main>
