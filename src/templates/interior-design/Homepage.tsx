bef0b9320e4e2e234c6863749220a7c82b6199ff
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-12-17 23:38:18 +0800

    add Testimonial1 to Homepage

--- src/templates/interior-design/Homepage.tsx
+++ src/templates/interior-design/Homepage.tsx
@@ -5,6 +5,7 @@ import LogoMarquee from '~/components/awards/LogoMarquee'
 // import Feature1 from '~/components/feature/Feature1'
 import Feature2 from '~/components/feature/Feature2'
 import Portfolio1 from '~/components/portfolio/portfolio1/Portfolio1'
+import Testimonials1 from '~/components/testimonials/testimonial1/Testimonials1'
 
 
 export default component$(() => {
@@ -16,6 +17,7 @@ export default component$(() => {
         {/* <Feature1 /> */}
         <Feature2 />
         <Portfolio1 />
+        <Testimonials1 />
       </main>
     </>
   )
