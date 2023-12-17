60e91018c96422a613e2de013e54227fcc459ae6
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2023-12-18 04:09:46 +0800

    delete Feature3, create Feature1

--- src/templates/interior-design/Homepage.tsx
+++ src/templates/interior-design/Homepage.tsx
@@ -2,8 +2,8 @@ import { component$ } from '@builder.io/qwik'
 
 import DoubleMarqueeHero from '~/components/hero/DoubleMarqueeHero/DoubleMarqueeHero'
 import LogoMarquee from '~/components/awards/LogoMarquee'
+import Feature1 from '~/components/feature/Feature1'
 import Feature2 from '~/components/feature/Feature2'
-import Feature3 from '~/components/feature/Feature3'
 import Portfolio1 from '~/components/portfolio/portfolio1/Portfolio1'
 import Testimonials1 from '~/components/testimonials/testimonial1/Testimonials1'
 import FoundersNote1 from '~/components/FoundersNote/FoundersNote1'
@@ -16,7 +16,7 @@ export default component$(() => {
       <DoubleMarqueeHero />
       <main>
         <LogoMarquee />
-        <Feature3 />
+        <Feature1 />
         <Feature2 />
         <Portfolio1 />
         <Testimonials1 />
