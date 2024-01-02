f57a79b063a92408dae30455467423aebf8be329
Siow Hao Zhen <96342348+HaoZhenSiow@users.noreply.github.com>
2024-01-02 15:37:48 +0800

    create Awards1

--- src/templates/interior-design/About.tsx
+++ src/templates/interior-design/About.tsx
@@ -1,12 +1,16 @@
 import { component$ } from '@builder.io/qwik'
+
 import Header1 from '~/components/header/Header1/Header1'
+import Story from './modified-components/Story'
+import Awards1 from '~/components/awards/Awards1'
 
 export default component$(() => {
   return (
     <>
       <Header1 />
       <main>
-
+        <Story />
+        <Awards1 />
       </main>
     </>
   )
